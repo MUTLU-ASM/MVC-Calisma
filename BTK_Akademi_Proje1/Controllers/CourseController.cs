227f95e868af6fcf5478f15ad5681fff2c981e76
Asim Mutlu <asimmutlu6134@gmail.com>
2023-07-05 16:26:51 +0300

    Add project files.

---
+++ BTK_Akademi_Proje1/Controllers/CourseController.cs
@@ -0,0 +1,29 @@
+﻿using BTK_Akademi_Proje1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTK_Akademi_Proje1.Controllers
+{
+    public class CourseController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        public IActionResult Apply()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Apply([FromForm] Candidate candidate)
+        {
+            return View();
+        }
+    }
+}
---
+++ BTK_Akademi_Proje1/Models/Candidate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BTK_Akademi_Proje1.Models
+{
+    public class Candidate
+    {
+        // String? bunun sonuna '?' koyulmasının sebebi boş geçilebilirdir.
+        public String? Email { get; set; } = String.Empty;
+        public String? FirstName { get; set; } = String.Empty;
+        public String? LastName { get; set; } = String.Empty;
+        public String? FullName => $"{FirstName} {LastName?.ToUpper()}";
+        public int? Age { get; set; }
+        public String? SelectedCourse { get; set; } = String.Empty;
+        public DateTime ApplyAt { get; set; }
+
+        public Candidate()
+        {
+            ApplyAt = DateTime.Now;
+        }
+
+    }
+}
